4e614a015e3ec2c05ab48fa63b4eaebde4f485eb
Atharva Parkale <xatharvaparkale@freo.money>
2025-11-05 12:28:24 +0530

    :memo: update UI for compose must know section

---
+++ app/src/main/java/com/example/kotlinlockin/common/data/CommonQuestions.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinlockin.common.data
+
+fun getAllCommonQuestions(): List<String> {
+    return listOf(
+        "What is the syntax for modifiers ?",
+        "How to iterate a list in compose and render UI ?",
+        "How to iterate a list in compose and render UI ?",
+        "What are the different types of classes in kotlin ?",
+        "What is suspend keyword in kotlin?",
+        "What is remember keyword in compose?",
+        "How to make an API call in compose ?",
+        "How to implement timer in compose ?",
+        "How to Parse the json in compose ?"
+    )
+}
---
+++ app/src/main/java/com/example/kotlinlockin/presentation/screens/view_model/domain/model/Post.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinlockin.presentation.screens.view_model.domain.model
+
+data class Post(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String,
+)
